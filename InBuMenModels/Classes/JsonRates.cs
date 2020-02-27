e69608a8e1719fa54fff0516d3ebe347c5ac2365
Sara Porcar <sararagon.5@live.com>
2020-02-27 09:22:55 +0100

    Clases de Json

---
+++ InBuMenModels/Classes/JsonRates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InBuMenModels.Classes
+{
+    public class RatesGroup
+    {
+        public Rates[] Property1 { get; set; }
+    }
+
+    public class Rates
+    {
+        [JsonProperty("from")]
+        public string From { get; set; }
+        [JsonProperty("to")]
+        public string To { get; set; }
+        [JsonProperty("rate")]
+        public string Rate { get; set; }
+    }
+
+}
---
+++ InBuMenModels/Classes/JsonTransactions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using Newtonsoft.Json;
+
+namespace InBuMenModels.Classes
+{
+    public class TransactionsGroup
+    {
+        public Transaction[] Property1 { get; set; }
+    }
+
+    public class Transaction
+    {
+        [JsonProperty("sku")]
+        public string Sku { get; set; }
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+    }
+
+}
