7bbb39c271d804e04d333de046f53fab4b79b56e
Sara Porcar <sararagon.5@live.com>
2020-02-27 12:32:59 +0100

    Comienzo Inyección de dependencias

--- InBuMenWebApi/App_Start/UnityConfig.cs
+++ InBuMenWebApi/App_Start/UnityConfig.cs
@@ -1,6 +1,9 @@
 using System.Web.Mvc;
+using InBuMenModels.Classes;
+using InBuMenModels.Interfaces;
 using Unity;
 using Unity.Mvc5;
+using WebApiInBuMen.Services.Repository;
 
 namespace InBuMenWebApi
 {
@@ -9,12 +12,17 @@ namespace InBuMenWebApi
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+
+            container.RegisterType<ICurrency, Currency>();
+            container.RegisterType<IJsonRate, JsonRate>();
+            container.RegisterType<IJsonRateGroup, JsonRateGroup>();
+            container.RegisterType<IJsonTransaction, JsonTransaction>();
+            container.RegisterType<IJsonTransactionGroup, JsonTransactionGroup>();
+            container.RegisterType<IRate, Rate>();
+            container.RegisterType<ITransaction, Transaction>();
+            container.RegisterType<IRepository<IJsonRate>, Repository<IJsonRate>>();
+            container.RegisterType<IRepository<IJsonTransaction>, Repository<IJsonTransaction>>();
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
--- InBuMenWebApi/Global.asax.cs
+++ InBuMenWebApi/Global.asax.cs
@@ -18,6 +18,8 @@ namespace InBuMenWebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            UnityConfig.RegisterComponents();
         }
     }
 }
